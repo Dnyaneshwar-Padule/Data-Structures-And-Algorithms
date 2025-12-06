fa8310f4a74eb5ea9c4b85916a537867cabc1677
Dnyaneshwar-Padule <dnyaneshwarpadule83@gmail.com>
2025-12-06 16:56:23 +0530

    DSA_in_Java: Linked Stack

---
+++ DSA_in_Java/Stack/02-Linked-Stack/src/com/tca/App.java
@@ -0,0 +1,25 @@
+package com.tca;
+
+import com.tca.util.LinkedStack;
+
+public class App {
+
+	public static void main(String[] args) {
+		LinkedStack<Integer> s = new LinkedStack<Integer>();
+		
+		try {
+			for(int i = 1; i < 30; ++i)
+				s.push(i);
+			
+			System.out.println("Peek : " + s.top());
+			
+			for(int i = 1; i <= 30; ++i) {
+				System.out.println("Pop : " + s.pop());
+			}
+		}
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
+}
---
+++ DSA_in_Java/Stack/02-Linked-Stack/src/com/tca/util/LinkedStack.java
@@ -0,0 +1,54 @@
+package com.tca.util;
+
+public class LinkedStack<T> {
+
+	private int size;
+	private ListNode top;
+	
+	private class ListNode{
+		T data;
+		ListNode next;
+		
+		ListNode(T data){
+			this.data = data;
+		}
+		
+		ListNode(T data, ListNode next){
+			this.data = data;
+			this.next = next;
+		}
+	}
+	
+	public LinkedStack() {
+		size = 0;
+		top = null;
+	}
+	
+	public boolean isEmpty() {
+		return top == null;
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public void push(T data) {
+		ListNode newNode = new ListNode(data, top);
+		top = newNode;
+	}
+	
+	public T pop()throws Exception {
+		if(isEmpty())
+			throw new Exception("Stack is empty.");
+		T data = top.data;
+		top = top.next;
+		return data;
+	}
+	
+	public T top() throws Exception{
+		if(isEmpty())
+			throw new Exception("Stack is empty.");
+		return top.data;
+	}
+	
+}
